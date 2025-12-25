286dca0f2d91d9345f53aa57f1a62bd852566925
Andrew Richmond <41588533+acrmine@users.noreply.github.com>
2025-12-24 21:14:08 -0800

    Locked in working platformer movement. Deleted animations though, need to add those back in with a better format.

--- main.ts
+++ main.ts
@@ -4,22 +4,29 @@ enum ActionKind {
     Jumping
 }
 function handlePlayerMovement () {
-    playerRobot.vx += controller.dx()
-    if (playerRobot.vx > 0) {
-        playerRobot.vx = Math.max(playerRobot.vx - linearDamping, 0)
-    } else if (playerRobot.vx < 0) {
-        playerRobot.vx = Math.min(playerRobot.vx + linearDamping, 0)
+    playerRobot.ay = gravity
+    if (controller.B.isPressed() && playerRobot.isHittingTile(CollisionDirection.Bottom)) {
+        playerRobot.vy = jumpPower
+    }
+    // Apply motion based on left right button being pressed, move faster when going in the opposite direction for quick turn around
+    if (controller.dx() / Math.abs(controller.dx()) != playerRobot.vx / Math.abs(playerRobot.vx)) {
+        playerRobot.vx += controller.dx() * (speedScalar * 5)
     } else {
-    	
+        playerRobot.vx += controller.dx() * speedScalar
+    }
+    // Apply linear damping to slow down player
+    if (Math.abs(playerRobot.vx) > maxSpeed || !(controller.anyButton.isPressed())) {
+        if (playerRobot.vx > 0) {
+            playerRobot.vx = Math.max(playerRobot.vx - linearDamping, 0)
+        } else if (playerRobot.vx < 0) {
+            playerRobot.vx = Math.min(playerRobot.vx + linearDamping, 0)
+        } else {
+        	
+        }
     }
 }
 function assemblePlayerAnims (mySprite: Sprite) {
     customUtils.setDataImageArray(mySprite, "playerIdle", [
-    assets.image`orange_robot_idle_0`,
-    assets.image`orange_robot_idle_1`,
-    assets.image`orange_robot_idle_2`,
-    assets.image`orange_robot_idle_3`,
-    assets.image`orange_robot_idle_4`
     ])
 }
 function assignPlayerAnims (mySprite: Sprite) {
@@ -31,8 +38,16 @@ function assignPlayerAnims (mySprite: Sprite) {
     )
 }
 let playerRobot: Sprite = null
+let jumpPower = 0
+let gravity = 0
+let maxSpeed = 0
+let speedScalar = 0
 let linearDamping = 0
-linearDamping = 1
+linearDamping = 12
+speedScalar = 2
+maxSpeed = 100
+gravity = 400
+jumpPower = -170
 let testLevel = tiles.createMap(tilemap`level1`)
 tiles.loadMap(testLevel)
 playerRobot = sprites.create(assets.image`orange_robot_idle_0`, SpriteKind.Player)
